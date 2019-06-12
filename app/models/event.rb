5ef053199d41682613c3d8bc5b4d639ef64ed5cb
Sebastian Poblete <sebapobletec@gmail.com>
2019-06-12 02:05:16 -0400

    dependent destroy

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :company
-  has_many :attendances
+  has_many :attendances, :dependent => :destroy
   has_many :workers, through: :attendances
 end
--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,5 +1,5 @@
 class Worker < ApplicationRecord
   belongs_to :company
-  has_many :attendances
+  has_many :attendances, :dependent => :destroy
   has_many :events, through: :attendances
 end
