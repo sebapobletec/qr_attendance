dfb30a9f266e0ab96e59d905cd880d09ea4080dc
Sebastian Poblete <sebapobletec@gmail.com>
2019-06-12 03:25:40 -0400

    edit active admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,31 +2,61 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    columns do
+
+      column do
+        panel 'Latest companies' do
+          ul do
+            Company.last(5).map do |company|
+              li link_to(company.name, admin_company_path(company))
+            end
+          end
+        end
       end
+
+      column do
+        panel 'Latest events' do
+          ul do
+            Event.last(5).map do |event|
+              li link_to(event.title, admin_event_path(event))
+            end
+          end
+        end
+      end
+
+      column do
+        panel 'Latest workers' do
+          ul do
+            Worker.last(5).map do |worker|
+              li link_to(worker.name, admin_worker_path(worker))
+            end
+          end
+        end
+      end
+
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    columns do
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
+      column do
+        panel 'Total companies' do
+          Company.count
+        end
+      end
+
+      column do
+        panel 'Total events' do
+          Event.count
+        end
+      end
+
+      column do
+        panel 'Total workers' do
+            Worker.count
+        end
+      end
+
+    end
+
   end # content
 end
