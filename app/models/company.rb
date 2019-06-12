a109d5e8fb9ace1839ae2c37b5fe7543223c9632
Sebastian Poblete <sebapobletec@gmail.com>
2019-06-12 00:39:22 -0400

    Migrate, edit routes and models

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,7 @@
 class Company < ApplicationRecord
   belongs_to :user
+  validates_uniqueness_of :name
+  validates_presence_of :name
+  validates_presence_of :rut
+  validates_presence_of :address 
 end
