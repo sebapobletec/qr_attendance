15f5823efdb5adcf93f56d623cf934bb4d61e840
Sebastian Poblete <sebapobletec@gmail.com>
2019-06-12 00:46:10 -0400

    edit company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,7 +1,9 @@
 class Company < ApplicationRecord
   belongs_to :user
+  has_many :workers, :dependent => :destroy
+  has_many :events, :dependent => :destroy
   validates_uniqueness_of :name
   validates_presence_of :name
   validates_presence_of :rut
-  validates_presence_of :address 
+  validates_presence_of :address
 end
